12b1a7b80b294218b00fb4abcf344ce60b9e2302
Sam Rose <hello@samwho.dev>
2026-05-20 16:26:58 +0100

    Create types.ts

---
+++ src/cluster/kubelet/images/types.ts
@@ -0,0 +1,32 @@
+import type { V1ObjectReference, V1Pod } from "../../../client";
+import type * as context from "../../../go/context";
+import type { PodSandboxConfig } from "../../cri";
+
+export class ImagePullError extends Error {
+	constructor(
+		reason:
+			| "ErrImagePull"
+			| "ImagePullBackOff"
+			| "ErrImageNeverPull"
+			| "InvalidImageName"
+			| "ErrImageInspect",
+		readonly messageForStatus: string,
+	) {
+		super(reason);
+		this.name = reason;
+	}
+}
+
+// Models kubernetes/pkg/kubelet/images/types.go ImageManager.
+export interface ImageManager {
+	ensureImageExists(
+		ctx: context.Context,
+		objRef: V1ObjectReference | undefined,
+		pod: V1Pod,
+		requestedImage: string,
+		pullSecrets: unknown[],
+		podSandboxConfig: PodSandboxConfig,
+		podRuntimeHandler: string,
+		pullPolicy: string | undefined,
+	): Promise<[imageRef: string, message: string, err: Error | undefined]>;
+}
