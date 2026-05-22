6a4860fe9f4c9e3e025eb51df8a3a42e5bba9e18
Sam Rose <hello@samwho.dev>
2026-05-22 17:09:06 +0100

    Create kuberuntime-sandbox.ts

---
+++ src/cluster/kubelet/kuberuntime/kuberuntime-sandbox.ts
@@ -0,0 +1,31 @@
+import type * as context from "../../../go/context";
+import { parseIP as parseIPSloppy } from "../../../go/net";
+import type { PodSandboxStatus } from "../../cri";
+
+// Models kubernetes/pkg/kubelet/kuberuntime/kuberuntime_sandbox.go kubeGenericRuntimeManager.determinePodSandboxIPs.
+export function determinePodSandboxIPs(
+	_ctx: context.Context,
+	_podNamespace: string,
+	_podName: string,
+	podSandbox: PodSandboxStatus,
+): string[] {
+	const podIPs: string[] = [];
+	if (!podSandbox.network) {
+		return podIPs;
+	}
+
+	if (podSandbox.network.ip.length !== 0) {
+		if (!parseIPSloppy(podSandbox.network.ip)) {
+			return [];
+		}
+		podIPs.push(podSandbox.network.ip);
+	}
+
+	for (const podIP of podSandbox.network.additionalIps ?? []) {
+		if (!parseIPSloppy(podIP.ip)) {
+			return [];
+		}
+		podIPs.push(podIP.ip);
+	}
+	return podIPs;
+}
