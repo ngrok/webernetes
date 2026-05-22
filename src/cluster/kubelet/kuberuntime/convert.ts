5abaec2795d01f4912b7115c208999e2320cdeb0
Sam Rose <hello@samwho.dev>
2026-05-22 16:58:46 +0100

    Create convert.ts

---
+++ src/cluster/kubelet/kuberuntime/convert.ts
@@ -0,0 +1,25 @@
+import type { Image as ImageFromCRI, ImageSpec as ImageSpecFromCRI } from "../../cri";
+import type { ImageSpec } from "../container";
+
+// Models kubernetes/pkg/kubelet/kuberuntime/convert.go ToKubeContainerImageSpec.
+export function toKubeContainerImageSpec(image: ImageFromCRI): ImageSpec {
+	const annotations = Object.entries(image.spec?.annotations ?? {})
+		.toSorted(([left], [right]) => left.localeCompare(right))
+		.map(([name, value]) => ({ name, value }));
+	return {
+		image: image.id,
+		runtimeHandler: image.spec?.runtimeHandler ?? "",
+		annotations,
+	};
+}
+
+// Models kubernetes/pkg/kubelet/kuberuntime/convert.go ToRuntimeAPIImageSpec.
+export function toRuntimeAPIImageSpec(image: ImageSpec): ImageSpecFromCRI {
+	return {
+		image: image.image,
+		runtimeHandler: image.runtimeHandler,
+		annotations: Object.fromEntries(
+			(image.annotations ?? []).map((annotation) => [annotation.name, annotation.value]),
+		),
+	};
+}
