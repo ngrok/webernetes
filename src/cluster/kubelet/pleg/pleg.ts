f8c17ebbcc56d27dcd39eeade0e80dc0dde1c100
Sam Rose <hello@samwho.dev>
2026-05-22 18:17:07 +0100

    Create pleg.ts

---
+++ src/cluster/kubelet/pleg/pleg.ts
@@ -0,0 +1,44 @@
+import type { ReadOnlyChannel } from "../../../go/channel";
+
+// Models kubernetes/pkg/kubelet/pleg/pleg.go PodLifeCycleEventType.
+export type PodLifeCycleEventType =
+	| "ContainerStarted"
+	| "ContainerDied"
+	| "ContainerRemoved"
+	| "PodSync"
+	| "ContainerChanged";
+
+export const ContainerStarted = "ContainerStarted" satisfies PodLifeCycleEventType;
+export const ContainerDied = "ContainerDied" satisfies PodLifeCycleEventType;
+export const ContainerRemoved = "ContainerRemoved" satisfies PodLifeCycleEventType;
+export const PodSync = "PodSync" satisfies PodLifeCycleEventType;
+export const ContainerChanged = "ContainerChanged" satisfies PodLifeCycleEventType;
+
+// Models kubernetes/pkg/kubelet/pleg/pleg.go RelistDuration.
+export interface RelistDuration {
+	relistPeriodMs: number;
+	relistThresholdMs: number;
+}
+
+// Models kubernetes/pkg/kubelet/pleg/pleg.go PodLifecycleEvent.
+export interface PodLifecycleEvent {
+	id: string;
+	type: PodLifeCycleEventType;
+	data?: string;
+}
+
+// Models kubernetes/pkg/kubelet/pleg/pleg.go PodLifecycleEventGenerator.
+export interface PodLifecycleEventGenerator {
+	start(): void;
+	watch(): ReadOnlyChannel<PodLifecycleEvent>;
+	healthy(): { ok: boolean; error?: Error };
+	requestReinspect(podUID: string): void;
+	requestRelist(podUID: string): void;
+}
+
+// Models kubernetes/pkg/kubelet/pleg/pleg.go podLifecycleEventGeneratorHandler.
+export interface PodLifecycleEventGeneratorHandler extends PodLifecycleEventGenerator {
+	stop(): Promise<void>;
+	update(relistDuration: RelistDuration): void;
+	relist(): void;
+}
