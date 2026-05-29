f3a743293b3bc98f50f528ff130cde6b7a9e1453
Sam Rose <hello@samwho.dev>
2026-05-29 14:45:35 +0100

    Add DNS validation.

---
+++ src/apimachinery/pkg/util/validation/validation.test.ts
@@ -0,0 +1,193 @@
+import { expect, it } from "vitest";
+import { browser } from "../../../../test/describe";
+import { isDNS1123Label, isDNS1123Subdomain } from "./validation";
+
+// Models staging/src/k8s.io/apimachinery/pkg/util/validation/validation_test.go TestIsDNS1123Label.
+browser.describe("isDNS1123Label", () => {
+	const goodValues = [
+		"a",
+		"ab",
+		"abc",
+		"a1",
+		"a-1",
+		"a--1--2--b",
+		"0",
+		"01",
+		"012",
+		"1a",
+		"1-a",
+		"1--a--b--2",
+		"a".repeat(63),
+	];
+
+	for (const value of goodValues) {
+		it(`accepts ${value}`, () => {
+			expect(isDNS1123Label(value)).toHaveLength(0);
+		});
+	}
+
+	const badValues = [
+		"",
+		"A",
+		"ABC",
+		"aBc",
+		"A1",
+		"A-1",
+		"1-A",
+		"-",
+		"a-",
+		"-a",
+		"1-",
+		"-1",
+		"_",
+		"a_",
+		"_a",
+		"a_b",
+		"1_",
+		"_1",
+		"1_2",
+		".",
+		"a.",
+		".a",
+		"a.b",
+		"1.",
+		".1",
+		"1.2",
+		" ",
+		"a ",
+		" a",
+		"a b",
+		"1 ",
+		" 1",
+		"1 2",
+		"a".repeat(64),
+	];
+
+	for (const value of badValues) {
+		it(`rejects ${value}`, () => {
+			expect(isDNS1123Label(value).length).toBeGreaterThan(0);
+		});
+	}
+});
+
+// Models staging/src/k8s.io/apimachinery/pkg/util/validation/validation_test.go TestIsDNS1123Subdomain.
+browser.describe("isDNS1123Subdomain", () => {
+	const goodValues = [
+		"a",
+		"ab",
+		"abc",
+		"a1",
+		"a-1",
+		"a--1--2--b",
+		"0",
+		"01",
+		"012",
+		"1a",
+		"1-a",
+		"1--a--b--2",
+		"a.a",
+		"ab.a",
+		"abc.a",
+		"a1.a",
+		"a-1.a",
+		"a--1--2--b.a",
+		"a.1",
+		"ab.1",
+		"abc.1",
+		"a1.1",
+		"a-1.1",
+		"a--1--2--b.1",
+		"0.a",
+		"01.a",
+		"012.a",
+		"1a.a",
+		"1-a.a",
+		"1--a--b--2",
+		"0.1",
+		"01.1",
+		"012.1",
+		"1a.1",
+		"1-a.1",
+		"1--a--b--2.1",
+		"a.b.c.d.e",
+		"aa.bb.cc.dd.ee",
+		"1.2.3.4.5",
+		"11.22.33.44.55",
+		"a".repeat(253),
+	];
+
+	for (const value of goodValues) {
+		it(`accepts ${value}`, () => {
+			expect(isDNS1123Subdomain(value)).toHaveLength(0);
+		});
+	}
+
+	const badValues = [
+		"",
+		"A",
+		"ABC",
+		"aBc",
+		"A1",
+		"A-1",
+		"1-A",
+		"-",
+		"a-",
+		"-a",
+		"1-",
+		"-1",
+		"_",
+		"a_",
+		"_a",
+		"a_b",
+		"1_",
+		"_1",
+		"1_2",
+		".",
+		"a.",
+		".a",
+		"a..b",
+		"1.",
+		".1",
+		"1..2",
+		" ",
+		"a ",
+		" a",
+		"a b",
+		"1 ",
+		" 1",
+		"1 2",
+		"A.a",
+		"aB.a",
+		"ab.A",
+		"A1.a",
+		"a1.A",
+		"A.1",
+		"aB.1",
+		"A1.1",
+		"1A.1",
+		"0.A",
+		"01.A",
+		"012.A",
+		"1A.a",
+		"1a.A",
+		"A.B.C.D.E",
+		"AA.BB.CC.DD.EE",
+		"a.B.c.d.e",
+		"aa.bB.cc.dd.ee",
+		"a@b",
+		"a,b",
+		"a_b",
+		"a;b",
+		"a:b",
+		"a%b",
+		"a?b",
+		"a$b",
+		"a".repeat(254),
+	];
+
+	for (const value of badValues) {
+		it(`rejects ${value}`, () => {
+			expect(isDNS1123Subdomain(value).length).toBeGreaterThan(0);
+		});
+	}
+});
---
+++ src/apimachinery/pkg/util/validation/validation.ts
@@ -0,0 +1,58 @@
+// Models staging/src/k8s.io/apimachinery/pkg/util/validation/validation.go dns1123LabelFmt.
+const dns1123LabelFmt = "[a-z0-9]([-a-z0-9]*[a-z0-9])?";
+const dns1123LabelRegexp = new RegExp(`^${dns1123LabelFmt}$`);
+const dns1123LabelErrMsg =
+	"a lowercase RFC 1123 label must consist of lower case alphanumeric characters or '-', and must start and end with an alphanumeric character";
+
+// Models staging/src/k8s.io/apimachinery/pkg/util/validation/validation.go DNS1123LabelMaxLength.
+const dns1123LabelMaxLength = 63;
+
+// Models staging/src/k8s.io/apimachinery/pkg/util/validation/validation.go dns1123SubdomainFmt.
+const dns1123SubdomainFmt = `${dns1123LabelFmt}(\\.${dns1123LabelFmt})*`;
+const dns1123SubdomainRegexp = new RegExp(`^${dns1123SubdomainFmt}$`);
+const dns1123SubdomainErrorMsg =
+	"a lowercase RFC 1123 subdomain must consist of lower case alphanumeric characters, '-' or '.', and must start and end with an alphanumeric character";
+
+// Models staging/src/k8s.io/apimachinery/pkg/util/validation/validation.go DNS1123SubdomainMaxLength.
+const dns1123SubdomainMaxLength = 253;
+
+// Models staging/src/k8s.io/apimachinery/pkg/util/validation/validation.go MaxLenError.
+function maxLenError(length: number): string {
+	return `must be no more than ${length} characters`;
+}
+
+// Models staging/src/k8s.io/apimachinery/pkg/util/validation/validation.go RegexError.
+function regexError(message: string, format: string, examples: string[]): string {
+	if (examples.length === 0) {
+		return `${message} (regex used for validation is '${format}')`;
+	}
+	return `${message} (e.g. ${examples.map((example) => `'${example}', `).join("or ")}regex used for validation is '${format}')`;
+}
+
+// Models staging/src/k8s.io/apimachinery/pkg/util/validation/validation.go IsDNS1123Label.
+export function isDNS1123Label(value: string): string[] {
+	const errors: string[] = [];
+	if (value.length > dns1123LabelMaxLength) {
+		errors.push(maxLenError(dns1123LabelMaxLength));
+	}
+	if (!dns1123LabelRegexp.test(value)) {
+		if (dns1123SubdomainRegexp.test(value)) {
+			errors.push("must not contain dots");
+		} else {
+			errors.push(regexError(dns1123LabelErrMsg, dns1123LabelFmt, ["my-name", "123-abc"]));
+		}
+	}
+	return errors;
+}
+
+// Models staging/src/k8s.io/apimachinery/pkg/util/validation/validation.go IsDNS1123Subdomain.
+export function isDNS1123Subdomain(value: string): string[] {
+	const errors: string[] = [];
+	if (value.length > dns1123SubdomainMaxLength) {
+		errors.push(maxLenError(dns1123SubdomainMaxLength));
+	}
+	if (!dns1123SubdomainRegexp.test(value)) {
+		errors.push(regexError(dns1123SubdomainErrorMsg, dns1123SubdomainFmt, ["example.com"]));
+	}
+	return errors;
+}
