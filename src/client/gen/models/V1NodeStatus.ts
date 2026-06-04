09dbd7ecf6a223b7e9a72df26e3591ff2e7c60b8
Sam Rose <hello@samwho.dev>
2026-06-04 14:17:05 +0100

    Update V1NodeStatus.ts

--- src/client/gen/models/V1NodeStatus.ts
+++ src/client/gen/models/V1NodeStatus.ts
@@ -1,9 +1,11 @@
 import { V1NodeAddress } from "./V1NodeAddress";
+import { V1NodeCondition } from "./V1NodeCondition";
 
 export interface V1NodeStatus {
 	addresses?: Array<V1NodeAddress>;
 	allocatable?: { [key: string]: string };
 	capacity?: { [key: string]: string };
+	conditions?: Array<V1NodeCondition>;
 	phase?: string;
 	volumesInUse?: Array<string>;
 }
