56ff19bb621f3d06dca7a6a712c174959143912b
Sam Rose <hello@samwho.dev>
2026-06-08 11:59:41 +0100

    Create path.ts

---
+++ src/apimachinery/pkg/api/validate/content/path.ts
@@ -0,0 +1,28 @@
+// Models staging/src/k8s.io/apimachinery/pkg/api/validate/content/path.go pathSegmentNameMayNotBe.
+const pathSegmentNameMayNotBe = [".", ".."];
+
+// Models staging/src/k8s.io/apimachinery/pkg/api/validate/content/path.go pathSegmentNameMayNotContain.
+const pathSegmentNameMayNotContain = ["/", "%"];
+
+// Models staging/src/k8s.io/apimachinery/pkg/api/validate/content/path.go IsPathSegmentName.
+export function isPathSegmentName(name: string): string[] {
+	for (const illegalName of pathSegmentNameMayNotBe) {
+		if (name === illegalName) {
+			return [`may not be '${illegalName}'`];
+		}
+	}
+
+	return isPathSegmentPrefix(name);
+}
+
+// Models staging/src/k8s.io/apimachinery/pkg/api/validate/content/path.go IsPathSegmentPrefix.
+export function isPathSegmentPrefix(name: string): string[] {
+	const errors: string[] = [];
+	for (const illegalContent of pathSegmentNameMayNotContain) {
+		if (name.includes(illegalContent)) {
+			errors.push(`may not contain '${illegalContent}'`);
+		}
+	}
+
+	return errors;
+}
