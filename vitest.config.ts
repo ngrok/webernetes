57487c414be920e3bad99da76c08f377d40dc4ad
Sam Rose <hello@samwho.dev>
2026-06-16 12:50:17 +0100

    Make sure test:node only runs what it should

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
 	test: {
 		environment: "node",
 		globalSetup: ["src/test/harnesses/kubernetes-k3s-global-setup.ts"],
-		include: ["src/**/*.test.ts"],
+		include: ["src/client/**/*.test.ts", "src/cluster/etcd.test.ts"],
 		passWithNoTests: true,
 		testTimeout: 240_000,
 		hookTimeout: 40_000,
