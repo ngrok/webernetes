c49e4420b4a0782c525d275a1678b0eee476eef4
Sam Rose <hello@samwho.dev>
2026-06-16 12:23:58 +0100

    Convert to deployments.

--- demo/src/setup.ts
+++ demo/src/setup.ts
@@ -9,101 +9,73 @@ export async function setup(cluster: w8s.Cluster): Promise<void> {
 	cluster.registerImage(DemoScheduledJobImage);
 
 	await cluster.apply([
-		{
-			apiVersion: "v1",
-			kind: "Pod",
-			metadata: {
-				name: "scheduled-job",
-				namespace: "default",
-				labels: { app: "scheduled-job", tier: "jobs" },
-			},
-			spec: {
-				containers: [
-					{
-						name: "scheduled-job",
-						image: "demo/scheduled-job:1.0",
+		deployment({
+			name: "scheduled-job",
+			labels: { app: "scheduled-job", tier: "jobs" },
+			containers: [
+				{
+					name: "scheduled-job",
+					image: "demo/scheduled-job:1.0",
+				},
+			],
+		}),
+		deployment({
+			name: "api",
+			labels: { app: "api", tier: "edge" },
+			containers: [
+				{
+					name: "api",
+					image: "demo/api:1.0",
+					ports: [{ name: "http", containerPort: 8080 }],
+					readinessProbe: {
+						httpGet: { path: "/readyz", port: "http" },
+						periodSeconds: 2,
+						failureThreshold: 1,
+						timeoutSeconds: 3,
 					},
-				],
-			},
-		},
-		{
-			apiVersion: "v1",
-			kind: "Pod",
-			metadata: {
-				name: "api",
-				namespace: "default",
-				labels: { app: "api", tier: "edge" },
-			},
-			spec: {
-				containers: [
-					{
-						name: "api",
-						image: "demo/api:1.0",
-						ports: [{ name: "http", containerPort: 8080 }],
-						readinessProbe: {
-							httpGet: { path: "/readyz", port: "http" },
-							periodSeconds: 2,
-							failureThreshold: 1,
-							timeoutSeconds: 3,
-						},
-						livenessProbe: {
-							httpGet: { path: "/healthz", port: "http" },
-							periodSeconds: 3,
-							failureThreshold: 2,
-							timeoutSeconds: 3,
-						},
+					livenessProbe: {
+						httpGet: { path: "/healthz", port: "http" },
+						periodSeconds: 3,
+						failureThreshold: 2,
+						timeoutSeconds: 3,
 					},
-				],
-			},
-		},
-		{
-			apiVersion: "v1",
-			kind: "Pod",
-			metadata: {
-				name: "database",
-				namespace: "default",
-				labels: { app: "database", tier: "data" },
-			},
-			spec: {
-				containers: [
-					{
-						name: "database",
-						image: "demo/database:1.0",
-						ports: [{ name: "http", containerPort: 5432 }],
-						readinessProbe: {
-							httpGet: { path: "/readyz", port: "http" },
-							periodSeconds: 2,
-							failureThreshold: 1,
-							timeoutSeconds: 3,
-						},
+				},
+			],
+		}),
+		deployment({
+			name: "database",
+			labels: { app: "database", tier: "data" },
+			containers: [
+				{
+					name: "database",
+					image: "demo/database:1.0",
+					ports: [{ name: "http", containerPort: 5432 }],
+					readinessProbe: {
+						httpGet: { path: "/readyz", port: "http" },
+						periodSeconds: 2,
+						failureThreshold: 1,
+						timeoutSeconds: 3,
 					},
-				],
-			},
-		},
-		{
-			apiVersion: "v1",
-			kind: "Pod",
-			metadata: {
-				name: "redis",
-				namespace: "default",
-				labels: { app: "redis", tier: "cache" },
-			},
-			spec: {
-				containers: [
-					{
-						name: "redis",
-						image: "demo/redis:1.0",
-						ports: [{ name: "http", containerPort: 6379 }],
-						readinessProbe: {
-							httpGet: { path: "/readyz", port: "http" },
-							periodSeconds: 2,
-							failureThreshold: 1,
-							timeoutSeconds: 3,
-						},
+				},
+			],
+		}),
+		deployment({
+			name: "redis",
+			labels: { app: "redis", tier: "cache" },
+			containers: [
+				{
+					name: "redis",
+					image: "demo/redis:1.0",
+					ports: [{ name: "http", containerPort: 6379 }],
+					readinessProbe: {
+						httpGet: { path: "/readyz", port: "http" },
+						periodSeconds: 2,
+						failureThreshold: 1,
+						timeoutSeconds: 3,
 					},
-				],
-			},
-		},
+				},
+			],
+		}),
 		{
 			apiVersion: "v1",
 			kind: "Service",
@@ -147,4 +119,40 @@ export async function setup(cluster: w8s.Cluster): Promise<void> {
 			},
 		},
 	]);
+}
+
+function deployment({
+	containers,
+	labels,
+	name,
+}: {
+	containers: w8s.V1Container[];
+	labels: Record<string, string>;
+	name: string;
+}): w8s.ClusterApplyResource {
+	return {
+		apiVersion: "apps/v1",
+		kind: "Deployment",
+		metadata: {
+			name,
+			namespace: "default",
+			labels,
+		},
+		spec: {
+			replicas: 1,
+			selector: {
+				matchLabels: {
+					app: labels.app,
+				},
+			},
+			template: {
+				metadata: {
+					labels,
+				},
+				spec: {
+					containers,
+				},
+			},
+		},
+	};
 }
