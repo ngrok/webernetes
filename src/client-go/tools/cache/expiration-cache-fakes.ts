0f42d7e44263d429718d76e2c1868cc589cfa8a7
Sam Rose <hello@samwho.dev>
2026-06-17 14:14:26 +0100

    Update expiration-cache-fakes.ts

--- src/client-go/tools/cache/expiration-cache-fakes.ts
+++ src/client-go/tools/cache/expiration-cache-fakes.ts
@@ -3,15 +3,29 @@
  * Derived from Kubernetes, translated and modified for Webernetes.
  */
 import type { PassiveClock } from "../../../utils/clock/clock";
-import { ExpirationCache, type ExpirationPolicy } from "./expiration-cache";
+import { ExpirationCache, type ExpirationPolicy, type TimestampedEntry } from "./expiration-cache";
 import type { KeyFunc, Store } from "./store";
+
+// Models staging/src/k8s.io/client-go/tools/cache/expiration_cache_fakes.go FakeExpirationPolicy.
+export class FakeExpirationPolicy<T> implements ExpirationPolicy<T> {
+	constructor(
+		readonly neverExpire: Set<string>,
+		readonly retrieveKeyFunc: (obj: TimestampedEntry<T>) => [string, Error | undefined],
+	) {}
+
+	// Models staging/src/k8s.io/client-go/tools/cache/expiration_cache_fakes.go FakeExpirationPolicy.IsExpired.
+	isExpired(obj: TimestampedEntry<T>): boolean {
+		const [key] = this.retrieveKeyFunc(obj);
+		return !this.neverExpire.has(key);
+	}
+}
 
 // Models staging/src/k8s.io/client-go/tools/cache/expiration_cache_fakes.go NewFakeExpirationStore.
 export function newFakeExpirationStore<T>(
 	keyFunc: KeyFunc<T>,
-	_deletedKeys: undefined,
+	deletedKeys: string[] | undefined,
 	expirationPolicy: ExpirationPolicy<T>,
 	cacheClock: PassiveClock,
 ): Store<T> {
-	return new ExpirationCache(keyFunc, cacheClock, expirationPolicy);
+	return new ExpirationCache(keyFunc, cacheClock, expirationPolicy, deletedKeys);
 }
